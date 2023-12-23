d45a0377fe6260461a35da9cbfea9d50dae0dbf4
Arno <arnobalois@hotmail.fr>
2023-12-23 13:52:40 +0100

    Menage

--- Laverie/Dashboard/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Machines, Locataires , Consomation
-
-# Register your models here.
-admin.site.register(Machines)
-admin.site.register(Locataires)
-admin.site.register(Consomation)
